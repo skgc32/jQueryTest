2b48db61da7dd4e46a7cc735c1d869e0200e36c9
skgc32 <191213129+skgc32@users.noreply.github.com>
2025-03-24 21:47:30 +0900

    [add]JavaScript 基礎文法

--- JavaScriptTest/app.js
+++ JavaScriptTest/app.js
@@ -1,1 +1,26 @@
-alert('Hello,World!');
+let input = prompt('数字を入力してください')
+
+fizzBuzz(input);
+
+function fizzBuzz(num) {
+    if (num % 15 === 0) {
+        alert('FizzBuzz');
+    } else if (num % 3 === 0) {
+        alert('Fizz');
+    } else if (num % 5 === 0) {
+        alert('Buzz');
+    } else {
+        alert(num);
+    }
+}
+
+let myname = 'John';
+
+let hello = greet(myname);
+
+console.log(hello);
+
+function greet(name){
+    let greeting = 'Hello,' + name;
+    return greeting;
+}
